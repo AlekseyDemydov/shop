e95f21a9098df040726486ef45843997f164c5e1
Олексій Демидов <agdemidof@gmail.com>
2023-03-02 20:57:11 +0200

    fix

--- src/pages/CatalogList/data.js
+++ src/pages/CatalogList/data.js
@@ -1,24 +1,27 @@
+import ford from './img/ford.jpg'
+import Chevrolet from './img/shevrole.jpg'
+import Lanos from './img/lanos.jpg'
 export const data = {
   productData: [
     {
       id: 1,
       title: 'Ford Mustang',
       price: '20000',
-      img: require('./img/ford.jpg'),
+      img: ford,
       text: 'Ford представив 454-сильний Mustang Mach 1 в Європі - Motorcar',
     },
     {
       id: 2,
       title: 'Chevrolet Cruze',
       price: '10000',
-      img: require('./img/shevrole.jpg'),
+      img: Chevrolet,
       text: 'Новый Шевроле Круз 2015 - Chevrolet Cruze 2015.',
     },
     {
       id: 3,
       title: 'Lanos',
       price: '15000',
-      img: require('./img/lanos.jpg'),
+      img: Lanos,
       text: 'Тюнинг Ланос: заказать запчасти в интернет-магазине - Автомобильный клуб Lanos (Сенс)',
     },
   ],
