1c3fc87da765d264d8bac149a0198839dc127006
Олексій Демидов <agdemidof@gmail.com>
2023-03-06 11:41:20 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,7 +14,7 @@ export const App = () => {
     <>
     <CartProvider>
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route path="/shop" element={<Layout />}>
           {/* <Route path="login" element={<LoginPage/>} /> */}
           <Route path="dashboard" element={<DashboardPage />}>
             <Route path='car' element={<Car/>}/>
